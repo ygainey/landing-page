b34e56fdc2e293c55070c2153bd15d694f5cc5de
YGainey <yousef.gainey@gmail.com>
2025-02-11 15:17:38 +0000

    footer template

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+      <footer className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto text-center">
+          <p>© 2025 Gas Engineers App. All rights reserved.</p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
