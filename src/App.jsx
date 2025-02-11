c0b8ab91b3f19dddd18f833e55c918606b187c78
YGainey <yousef.gainey@gmail.com>
2025-02-11 15:11:06 +0000

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,22 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import About from "./components/About";
+import QRSection from "./components/QRSection.jsx";
+import Footer from "./components/Footer";
 
+function App() {
+  return (
+    <div className="font-sans">
+      <Header />
+      <Hero />
+      <Features />
+      <About />
+      <QRSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
 
